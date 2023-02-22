b113058bccb2e296c6994e32be1cfb263abf2e73
Zander <114107976+Femnyx@users.noreply.github.com>
2023-02-22 14:22:47 -0600

    Initial commit

---
+++ HelloWorld_Mcvey_Zander/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld_Mcvey_Zander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HelloWorld_Mcvey_Zander
+ 
