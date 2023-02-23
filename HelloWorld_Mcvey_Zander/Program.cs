e834268ae60a79fa0dbfc628d6c3930e27f37431
Zander <114107976+Femnyx@users.noreply.github.com>
2023-02-23 13:09:44 -0600

    Update 1

--- HelloWorld_Mcvey_Zander/Program.cs
+++ HelloWorld_Mcvey_Zander/Program.cs
@@ -5,8 +5,16 @@ namespace HelloWorld_Mcvey_Zander
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {/* Ask for the User's firstName */
+            Console.WriteLine("Hello, what is your first name Stranger?");
+             string firstName = Console.ReadLine();
+         /* Next, ask for the User's lastName*/
+            Console.WriteLine("Interesting, What is your last name?");
+             string lastName = Console.ReadLine();
+         /* Make it seem like you sorta know this person*/
+            Console.WriteLine($"{firstName} {lastName}, yes, you seem...Familiar");
+
+            
         }
     }
 }
